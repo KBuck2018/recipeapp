41061c0a378348d2708bc62a5938f5095fcfb095
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-03-05 12:40:02 -0500

    added all installs to package for passport

--- index.js
+++ index.js
@@ -4,26 +4,31 @@ const app = express();
 const bodyParser = require("body-parser");
 const hbs = require("hbs");
 const methodOverride = require("method-override");
-// const session = require("express-session");
-// const passport = require("passport");
+const session = require("express-session");
+const passport = require("passport");
 
 //This is a reference to the controller.js in controllers, ensures functionality of the MVC
 const recipe = require("./controllers/recipe");
 var Recipe = require("./models/Recipe");
 
-// const flash = require("connect-flash");
-// const cookieParser = require("cookie-parser");
-// const morgan = require("morgan");
+const flash = require("connect-flash");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(morgan("dev"));
-//This is the beginning reference to the views/index.hbs, anything put in that file and layout will show up here
+app.use(morgan("dev"));
 
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(bodyParser());
-
-// app.use(flash());
+app.use(
+  session({
+    secret: "ilovescotchscotchyscotchscotch"
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(methodOverride("_method"));
 app.use("/recipes", recipe);
